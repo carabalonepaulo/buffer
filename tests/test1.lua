1262844225baf3a251f0ac214432da87852a2122
Paulo Carabalone <psoreto@outlook.com>
2017-09-15 11:41:55 -0300

    Update test1.lua

--- tests/test1.lua
+++ tests/test1.lua
@@ -1,28 +1,28 @@
 require 'buffer'
 
-function assertEquals(value, excpected, message)
+function assertequals(value, excpected, message)
   if value ~= excpected then
     print(message)
   end
 end
 
 local writer = BinaryWriter:new()
-writer:writeByte(20)                -- 1
-writer:writeInt(1267)               -- 4
-writer:writeFloat(1.23)             -- 4
-writer:writeDouble(1.18923764)      -- 8
-writer:writeString('Paulo Soreto')  -- 4 + 12
+writer:writebyte(20)                -- 1
+writer:writeint(1267)               -- 4
+writer:writefloat(1.23)             -- 4
+writer:writedouble(1.18923764)      -- 8
+writer:writestring('Paulo Soreto')  -- 4 + 12
 
 assertEquals(writer:getSize(), 33, 'size :(')
 
 local reader = BinaryReader:new(writer)
-assertEquals(reader:readByte(), 20, 'byte :(')
-assertEquals(reader:readInt(), 1267, 'int :(')
+assertequals(reader:readbyte(), 20, 'byte :(')
+assertequals(reader:readint(), 1267, 'int :(')
 
-local f = reader:readFloat()
-assertEquals((math.max(f, 1.23)), f, 'float :(')
+local f = reader:readfloat()
+assertequals((math.max(f, 1.23)), f, 'float :(')
 
-local d = reader:readDouble()
-assertEquals(math.max(d, 1.23), d, 'double :(')
+local d = reader:readdouble()
+assertequals(math.max(d, 1.23), d, 'double :(')
 
-assertEquals(reader:readString(), 'Paulo Soreto', 'string :(')
+assertequals(reader:readstring(), 'Paulo Soreto', 'string :(')
